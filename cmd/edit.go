92f8bf3f5cb96ea1ee53c5d62aacbb4785f5d725
ShrewdSpirit <mohammad.zardoshtian@ymail.com>
2018-12-15 03:31:20 +0330

    implemented editing

--- cmd/edit.go
+++ cmd/edit.go
@@ -1,50 +1,109 @@
-// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package cmd
 
 import (
+	"bytes"
 	"fmt"
+	"strings"
 
+	"github.com/ShrewdSpirit/credman/config"
 	"github.com/spf13/cobra"
 )
 
-// editCmd represents the edit command
 var editCmd = &cobra.Command{
 	Use:   "edit",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Edit site fields",
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("edit called")
+		siteName := args[0]
+
+		profileName, _ := cmd.Flags().GetString("profile")
+		if profileName == "" {
+			profileName = config.AppConfig.DefaultProfile
+		}
+		if profileName == "" {
+			fmt.Println("Create a profile first")
+			return
+		}
+
+		if Verbose {
+			fmt.Println("Reading profile")
+		}
+		profile, err := config.GetProfile(ProfilesDir, profileName)
+		if profile == nil && err != nil {
+			fmt.Printf("No such profile '%s'\n", profileName)
+			return
+		}
+
+		profilePassword, err := PromptPassword("Profile password")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if Verbose {
+			fmt.Println("Decrypting profile")
+		}
+		err = profile.Decrypt(profilePassword)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if Verbose {
+			fmt.Println("Checking site's existence")
+		}
+		site := profile.Site(siteName)
+		if site == nil {
+			fmt.Printf("Site '%s' doesn't exist\n", siteName)
+			return
+		}
+
+		if Verbose {
+			fmt.Println("Setting fields")
+		}
+
+		email, _ := cmd.Flags().GetString("email")
+		username, _ := cmd.Flags().GetString("username")
+
+		fields, _ := cmd.Flags().GetStringToString("fields")
+
+		if len(email) > 0 {
+			fields["email"] = email
+		}
+		if len(username) > 0 {
+			fields["username"] = username
+		}
+
+		setFieldsBuffer := bytes.Buffer{}
+		for field, value := range fields {
+			site.Fields[field] = value
+			setFieldsBuffer.WriteString(field + ", ")
+		}
+		setFields := setFieldsBuffer.String()
+		setFields = strings.TrimSuffix(setFields, ", ")
+
+		if Verbose {
+			fmt.Println("Encrypting profile")
+		}
+		if err := profile.Encrypt(profilePassword); err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if Verbose {
+			fmt.Println("Saving profile")
+		}
+		if err := profile.Save(ProfilesDir); err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Printf("Fields %s has been updated\n", setFields)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(editCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// editCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// editCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	AddFlagsProfileName(editCmd)
+	AddFlagsSiteFields(editCmd, false)
 }
