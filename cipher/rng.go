68b1b9d9c0dbe1a088a68e31228742fbb90a09c1
ShrewdSpirit <mohammad.zardoshtian@hotmail.com>
2019-02-20 17:57:58 +0330

    use crypto/rand

--- cipher/rng.go
+++ cipher/rng.go
@@ -1,25 +1,17 @@
 package cipher
 
 import (
-	"math/rand"
-	"time"
+	"crypto/rand"
 )
-
-var randSource = rand.NewSource(time.Now().UnixNano())
-var rng = rand.New(randSource)
-
-func RandRange(min, max int64) int64 {
-	return min + rng.Int63()%max
-}
 
 func GenerateSalt(len int) []byte {
 	salt := make([]byte, len)
-	rng.Read(salt)
+	rand.Read(salt)
 	return salt
 }
 
 func GenerateIV() []byte {
 	iv := make([]byte, 16)
-	rng.Read(iv)
+	rand.Read(iv)
 	return iv
 }
