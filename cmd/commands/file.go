1e880310a200e5f1c9f3cf6f2d8f940318720c80
ShrewdSpirit <mohammad.zardoshtian@hotmail.com>
2019-05-30 03:08:26 +0430

    refactored file enc/dec

--- cmd/commands/file.go
+++ cmd/commands/file.go
@@ -1,8 +1,11 @@
 package commands
 
 import (
+	"os"
+	"strings"
+
+	"github.com/ShrewdSpirit/credman/cipher"
 	"github.com/ShrewdSpirit/credman/cmd/cmdutility"
-	"github.com/ShrewdSpirit/credman/management"
 	"github.com/spf13/cobra"
 )
 
@@ -17,49 +20,7 @@ var fileEncryptCmd = &cobra.Command{
 	Aliases: []string{"enc", "e"},
 	Args:    cobra.ExactArgs(1),
 	Short:   "Encrypt file",
-	Run: func(cmd *cobra.Command, args []string) {
-		inputFilename := args[0]
-
-		management.FileData{
-			InputFilename:  inputFilename,
-			OutputFilename: &fileOutput,
-			DeleteInput:    fileDeleteOriginal,
-			SaveSite:       !fileNoNewSite,
-			StoreFile:      fileStore,
-			PasswordReader: func(step management.ManagementStep) string {
-				password, err := cmdutility.NewPasswordPrompt("New password")
-				if err != nil {
-					cmdutility.LogError("Failed reading password", err)
-					return ""
-				}
-				return password
-			},
-			ManagementData: management.ManagementData{
-				OnStep: func(step management.ManagementStep) {
-					switch step {
-					case management.FileStepEncrypting:
-						cmdutility.LogColor(cmdutility.Green, "Encrypting file %s", inputFilename)
-					case management.StepDone:
-						cmdutility.LogColor(cmdutility.Green, "Encryption done. Wrote data to %s", fileOutput)
-					case management.FileStepDeletingInput:
-						cmdutility.LogColor(cmdutility.Green, "Deleting original file %s", inputFilename)
-					}
-				},
-				OnError: func(step management.ManagementStep, err error) {
-					switch step {
-					case management.FileStepOpeningInput:
-						cmdutility.LogError("Failed to open input file", err)
-					case management.FileStepCreatingOutput:
-						cmdutility.LogError("Failed to create output file", err)
-					case management.FileStepEncrypting:
-						cmdutility.LogError("Failed to encrypt file", err)
-					case management.FileStepDeletingInput:
-						cmdutility.LogError("Failed to delete original file", err)
-					}
-				},
-			},
-		}.Encrypt()
-	},
+	Run:     fileEncrypt,
 }
 
 var fileDecryptCmd = &cobra.Command{
@@ -67,47 +28,10 @@ var fileDecryptCmd = &cobra.Command{
 	Aliases: []string{"dec", "d"},
 	Args:    cobra.ExactArgs(1),
 	Short:   "Decrypt file",
-	Run: func(cmd *cobra.Command, args []string) {
-		inputFilename := args[0]
-
-		management.FileData{
-			InputFilename:  inputFilename,
-			OutputFilename: &fileOutput,
-			PasswordReader: func(step management.ManagementStep) string {
-				password, err := cmdutility.PasswordPrompt("File password")
-				if err != nil {
-					cmdutility.LogError("Failed reading password", err)
-					return ""
-				}
-				return password
-			},
-			ManagementData: management.ManagementData{
-				OnStep: func(step management.ManagementStep) {
-					switch step {
-					case management.FileStepInvalidInput:
-						cmdutility.LogColor(cmdutility.BoldRed, "Input file name %s is same as output %s", inputFilename, fileOutput)
-					case management.FileStepDecrypting:
-						cmdutility.LogColor(cmdutility.Green, "Decrypting file %s", inputFilename)
-					case management.StepDone:
-						cmdutility.LogColor(cmdutility.Green, "Decryption done. Wrote data to %s", fileOutput)
-					}
-				},
-				OnError: func(step management.ManagementStep, err error) {
-					switch step {
-					case management.FileStepOpeningInput:
-						cmdutility.LogError("Failed to open input file", err)
-					case management.FileStepCreatingOutput:
-						cmdutility.LogError("Failed to create output file", err)
-					case management.FileStepDecrypting:
-						cmdutility.LogError("Failed to decrypt file", err)
-					}
-				},
-			},
-		}.Decrypt()
-	},
+	Run:     fileDecrypt,
 }
 
-var fileOutput string
+var outputFilename string
 var fileDeleteOriginal bool
 var fileNoNewSite bool
 var fileStore bool
@@ -126,5 +50,100 @@ func init() {
 }
 
 func fileFlagsAddOutput(cmd *cobra.Command) {
-	cmd.Flags().StringVarP(&fileOutput, "output", "o", "", "Output file for encryption/decryption")
+	cmd.Flags().StringVarP(&outputFilename, "output", "o", "", "Output file for encryption/decryption")
+}
+
+func fileEncrypt(cmd *cobra.Command, args []string) {
+	inputFilename := args[0]
+
+	if _, err := os.Stat(inputFilename); err != nil {
+		cmdutility.LogError("File doesn't exist", err)
+		return
+	}
+
+	if len(outputFilename) == 0 {
+		outputFilename = inputFilename + ".enc"
+	}
+
+	password, err := cmdutility.NewPasswordPrompt("New password")
+	if err != nil {
+		cmdutility.LogError("Failed reading password", err)
+		return
+	}
+
+	inputFile, err := os.Open(inputFilename)
+	if err != nil {
+		cmdutility.LogError("Failed to open input file", err)
+		return
+	}
+
+	if !fileStore {
+		outputFile, err := os.Create(outputFilename)
+		if err != nil {
+			cmdutility.LogError("Failed to create output file", err)
+			return
+		}
+
+		cmdutility.LogColor(cmdutility.Green, "Encrypting file %s", inputFilename)
+		if err := cipher.StreamEncrypt(inputFile, outputFile, password); err != nil {
+			os.Remove(outputFilename)
+			cmdutility.LogError("Failed to encrypt file", err)
+			return
+		}
+	} // TODO: store in site
+
+	if fileDeleteOriginal {
+		cmdutility.LogColor(cmdutility.Green, "Deleting original file %s", inputFilename)
+		if err := os.Remove(inputFilename); err != nil {
+			cmdutility.LogError("Failed to delete original file", err)
+			return
+		}
+	}
+
+	cmdutility.LogColor(cmdutility.Green, "Encryption done. Wrote data to %s", outputFilename)
+}
+
+func fileDecrypt(cmd *cobra.Command, args []string) {
+	inputFilename := args[0]
+
+	if _, err := os.Stat(inputFilename); err != nil {
+		cmdutility.LogError("File doesn't exist", err)
+		return
+	}
+
+	if len(outputFilename) == 0 {
+		outputFilename = strings.TrimSuffix(inputFilename, ".enc")
+	}
+
+	if outputFilename == inputFilename {
+		cmdutility.LogColor(cmdutility.BoldRed, "Input file name %s is same as output %s", inputFilename, outputFilename)
+		return
+	}
+
+	password, err := cmdutility.PasswordPrompt("File password")
+	if err != nil {
+		cmdutility.LogError("Failed reading password", err)
+		return
+	}
+
+	inputFile, err := os.Open(inputFilename)
+	if err != nil {
+		cmdutility.LogError("Failed to open input file", err)
+		return
+	}
+
+	outputFile, err := os.Create(outputFilename)
+	if err != nil {
+		cmdutility.LogError("Failed to create output file", err)
+		return
+	}
+
+	cmdutility.LogColor(cmdutility.Green, "Decrypting file %s", inputFilename)
+	if err := cipher.StreamDecrypt(inputFile, outputFile, password); err != nil {
+		os.Remove(outputFilename)
+		cmdutility.LogError("Failed to decrypt file", err)
+		return
+	}
+
+	cmdutility.LogColor(cmdutility.Green, "Decryption done. Wrote data to %s", outputFilename)
 }
